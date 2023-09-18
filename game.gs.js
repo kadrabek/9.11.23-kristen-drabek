928d041a4e2131a3c877a7dfc503a12aeb5b57b6
Kristen Drabek <144624966+kadrabek@users.noreply.github.com>
2023-09-17 20:37:34 -0700

    Add files via upload

---
+++ game.gs.js
@@ -0,0 +1,14 @@
+//the function code allows us to create a function for the func that we added in our html. 
+function funcAlert()
+
+//the meat of the code goes into the curly brackets. without these the function will not work.
+{
+
+//this add a variable to our code and it will generate a number from 1-6 randomly.
+    var randNum1 = Math.ceil(Math.random()*6);
+
+//this alert will pop up when you click on the button we added in the html document. the random number variable we added will pop up here alongside the message in quotes. 
+    alert("your number = " + randNum1);
+    
+//this lets the computer know that the function is done and ready to go be booted up. 
+}
